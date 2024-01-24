c4ae511282716a9dbb9c37419cc9ad182ad5ad84
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-01-24 20:44:52 +0530

    Today Learnt about lists

---
+++ basics/list.py
@@ -0,0 +1,12 @@
+lt1 = [1,2,3,4,5]
+#   print(lt1)
+
+lt2 = [1,2,34,5.67,-8,'a',"Shubham","Any Defines Object"]
+#   print(lt2) 
+
+lt6 = ["We can also traverse and choose particular objs"]
+print()
+lt3 = [9,8,7,lt1,lt2,lt6]
+print(lt3)
+print()
+print(lt3[2], lt3[3][4], lt3[4][6],lt3[5])
