1c28d096d615e99901d23253b793f5f30b985851
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-01-25 10:28:34 +0530

    Done with dt

--- basics/dict.py
+++ basics/dict.py
@@ -1,2 +1,5 @@
 # Dictonary -- it is a unordered collection of key value pairs.
 
+dt = {'name':'Shubham Adarsh','branch':'CSE-AIML','year':'VIII Sem','roll':'53'}
+print(dt)
+print(dt['year'],'\n',dt['name'])
