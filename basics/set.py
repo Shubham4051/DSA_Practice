f0efbb8a9afc2ab8bf85b1a0e9689816bd282e7e
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-01-25 10:33:35 +0530

    Set used as dt write as lt

---
+++ basics/set.py
@@ -0,0 +1,4 @@
+# Set - Set is initialized as dictonary and written as list.
+st = {'Shubham@gmail.com',5,6,'Adarsh@gmail.com','Shubham@gmail.com','Adarsh@gmail.com','Shubham_Adarsh@gmail.com',5,7,7,'ok'}
+print(st)
+# Set use to pop unique elements from the list patterned as dictonary. 
