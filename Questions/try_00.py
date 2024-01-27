13acb174af5be9d551ff993986e96b5e90c93014
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-01-28 01:12:52 +0530

    Made a program to Print the Nature of a number.

---
+++ Questions/try_00.py
@@ -0,0 +1,9 @@
+# To print nature of the numbers :--
+
+num = int(input('Enter The Number -- '))
+if num >= 0:
+    print('Number is Positive.')
+elif num <= 0:
+    print('Number is Negative.')
+else:
+    print('Number is 0.')
