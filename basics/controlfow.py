271e5bcc815190a18f17a554873c1f4ad0890b5c
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-01-28 01:37:10 +0530

    made changes in controlflow program.

--- basics/controlfow.py
+++ basics/controlfow.py
@@ -1,3 +1,11 @@
 # Comparision operators "==", ">=", "<=", "!=", "<", ">"
 # Logical operator "and", "or", "not"
 # If Elif Else 
+
+
+num = int(input('Enter a number '))
+for i in range(num):
+    i += 1
+    print(i, end=' ')
+
+# The Program Made above is to show how to print in a single line.
---
+++ basics/loops.py
@@ -0,0 +1,6 @@
+# 1 For loop 
+lt = ['Shubham', 'Prabhash', 'Lukman', 'Vishal', 'Raman']
+for temp in lt:
+    print(temp)
+else:
+    print('We are Done!!')
