57352a8c5f1cce7d5eab2e93dce33e51846ee7bb
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-01-28 11:37:03 +0530

    while loop intro

--- basics/loops.py
+++ basics/loops.py
@@ -1,6 +1,18 @@
 # 1 For loop 
+
+"""
 lt = ['Shubham', 'Prabhash', 'Lukman', 'Vishal', 'Raman']
 for temp in lt:
     print(temp)
 else:
     print('We are Done!!')
+
+"""
+
+# 2 While loop
+
+want_to_kiss = 'y'
+while want_to_kiss == 'y':
+    want_to_kiss = input('kissing (y/n) ')
+else:
+    print('Kyu pht gyi ?')
