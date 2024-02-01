15e4b9b76b07ca89e281a06a78e832266017d646
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-01 17:56:36 +0530

    Learning functions

---
+++ basics/functions.py
@@ -0,0 +1,9 @@
+# Functions - code reusability when written once.
+
+def talk():
+    print("Hello !")
+    print("Studing well ?")
+    print("You belive in yourself !")
+    print('You can do it !')
+talk()
+
