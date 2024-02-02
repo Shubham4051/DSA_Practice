b2197cbc0bf9041928cdfc69b480a8a32908b818
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-02 21:53:48 +0530

    Functions in more detail

--- basics/functions.py
+++ basics/functions.py
@@ -1,9 +1,23 @@
 # Functions - code reusability when written once.
 
-def talk():
-    print("Hello !")
-    print("Studing well ?")
-    print("You belive in yourself !")
-    print('You can do it !')
-talk()
+def talk(name):
+    print("Hello, ", name)
+    print(name, "Studing well ?")
+    print(name, "You belive in yourself !")
+    print('You can do it ?', name)
 
+
+def ans(he):
+    print("Hi !",he)
+    print("Trying my best ", he)
+    print("Yes I believe in Myself")
+    print("Yes definitly, I can and I will do it !")
+    print("Thanks !",he)
+
+
+
+talk('Shubham')
+
+print("\n\n")
+
+ans("Future Me")
