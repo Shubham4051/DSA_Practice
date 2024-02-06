920244b9cc2eb904c290972726e06a678fa8a634
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-06 21:13:34 +0530

    learning to make documentation of libs

---
+++ basics/doc.py
@@ -0,0 +1,10 @@
+def doc():
+    ''' 
+This is a simple program made to just ouptput your name
+
+    this is a documentation file and will pop when help is called 
+
+    so hows your progress is going please stay focused and you will have a good days sooonnnnn.
+    '''
+
+help(doc)
