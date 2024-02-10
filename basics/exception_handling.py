24363d08427a3f9de4112cab4e505e17ac7885f6
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-10 21:17:34 +0530

    exception handling with csv file

---
+++ basics/exception_handling.py
@@ -0,0 +1,8 @@
+# try:
+#     print(1/1)
+# except:
+#     print("This will nor work")
+# finally: # it will work even if nothing works
+#     print("Ho gya kya ???")
+
+fl_new = open("nwe_file_2.csv", 'w')
