51cae2dac16c9f984c725a761348b3a879dfbd57
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-13 02:49:53 +0530

    relocated

--- 100+/3_ar_tr.py
+++ 100+/3_ar_tr.py
@@ -1,9 +1,4 @@
-# # Area of Right triangle 
-# h,b= map(float,input("Enter h and b\n").split())
-# ar = 1/2*(h*b)
-# print(ar)
-x = 15
-if x > 15:
-   print ('go')
-if x < 20:
-   print ('stop')
+# Area of Right triangle 
+h,b= map(float,input("Enter h and b\n").split())
+ar = 1/2*(h*b)
+print(ar)
