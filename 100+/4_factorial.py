398e349e60e02be888a7b2861604263c0c5c9856
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-03-05 01:36:00 +0530

    comple factorial

--- 100+/4_factorial.py
+++ 100+/4_factorial.py
@@ -1,6 +1,11 @@
 n = int(input())
-i = 1
-while n > 1:
-    i = n*i
-    n -= 1
-print(i)
+if n < 0:
+    print("Factorial of -ve no. is not possible")
+elif n == 0:
+    print("1")
+else:
+    i = 1
+    while n > 1:
+        i = n*i
+        n -= 1
+    print(i)
